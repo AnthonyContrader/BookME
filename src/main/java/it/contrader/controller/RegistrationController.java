d7cd6da12023b1779cd950430e6ec2f7bb8b4181
Manu <manu.scazza@hotmail.it>
2019-07-02 16:32:15 +0200

    register

---
+++ src/main/java/it/contrader/controller/RegistrationController.java
@@ -0,0 +1,38 @@
+package it.contrader.controller;
+
+import it.contrader.dto.UserDTO;
+import it.contrader.main.MainDispatcher;
+import it.contrader.service.UserService;
+
+public class RegistrationController implements Controller{
+
+	private String username, password;
+	private final String usertype = "user"; 
+	private UserService userService;
+	
+	public RegistrationController() {
+		userService = new UserService();
+	}
+	
+	public void doControl(Request request) {
+		if (request != null) {
+
+			username = request.get("username").toString();
+			password = request.get("password").toString();
+			
+			//costruisce l'oggetto user da inserire
+			UserDTO usertoinsert = new UserDTO(username, password, usertype);
+			//invoca il service
+			userService.insert(usertoinsert);
+			request = new Request();
+			request.put("mode", "mode");
+			//Rimanda alla view con la risposta
+			MainDispatcher.getInstance().callView("Registration", request);
+		
+					
+		}
+		else{
+			MainDispatcher.getInstance().callView("Login", null);
+		}
+	}
+}
