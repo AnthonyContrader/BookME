62682fbdf6c3193d962d35a3e5a0ab80c794ec89
Artem contrader <a.matyushin@contrader.it>
2019-07-04 11:35:53 +0200

    Libro controller e model

---
+++ src/main/java/it/contrader/model/Libro.java
@@ -0,0 +1,56 @@
+package it.contrader.model;
+
+public class Libro {
+	
+	
+	private int id_Libro;
+	
+	private String nome_Libro;
+	
+	private int id_Categoria;
+	
+	
+
+	public Libro(int id_Libro, String nome_Libro, int id_Categoria) {
+		this.id_Libro = id_Libro;
+		this.nome_Libro = nome_Libro;
+		this.id_Categoria = id_Categoria;
+	}
+	
+
+	public int getId_Libro() {
+		return id_Libro;
+	}
+
+	public void setId_Libro(int id_Libro) {
+		this.id_Libro = id_Libro;
+	}
+
+	public String getNome_Libro() {
+		return nome_Libro;
+	}
+
+	public void setNome_Libro(String nome_Libro) {
+		this.nome_Libro = nome_Libro;
+	}
+
+	public int getId_Categoria() {
+		return id_Categoria;
+	}
+
+	public void setId_Categoria(int id_Categoria) {
+		this.id_Categoria = id_Categoria;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Libro [id_Libro=" + id_Libro + ", nome_Libro=" + nome_Libro + ", id_Categoria=" + id_Categoria + "]";
+	}
+	
+	
+	
+	
+	
+
+}
