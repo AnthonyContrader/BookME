8da62f7f9706d505d9b5d9f1da7b776ea63f121c
Giacomo <giacomo.arminio.yahoo.com>
2019-07-11 16:09:39 +0200

    boooh

---
+++ db/sampledbold.sql
@@ -0,0 +1,161 @@
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: sampledb
+-- ------------------------------------------------------
+-- Server version	8.0.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categorie`
+--
+
+DROP TABLE IF EXISTS `categorie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `categorie` (
+  `id` int(10) unsigned NOT NULL,
+  `nome_Categorie` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorie`
+--
+
+LOCK TABLES `categorie` WRITE;
+/*!40000 ALTER TABLE `categorie` DISABLE KEYS */;
+INSERT INTO `categorie` VALUES (1,'Horror'),(2,'Fantasy');
+/*!40000 ALTER TABLE `categorie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `libro`
+--
+
+DROP TABLE IF EXISTS `libro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `libro` (
+  `id_Libro` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_Libro` varchar(45) DEFAULT NULL,
+  `id_Categoria` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id_Libro`),
+  KEY `idcategoria_idx` (`id_Categoria`),
+  CONSTRAINT `idcategoria` FOREIGN KEY (`id_Categoria`) REFERENCES `categorie` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libro`
+--
+
+LOCK TABLES `libro` WRITE;
+/*!40000 ALTER TABLE `libro` DISABLE KEYS */;
+INSERT INTO `libro` VALUES (1,'merda',1);
+/*!40000 ALTER TABLE `libro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personaggi`
+--
+
+DROP TABLE IF EXISTS `personaggi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `personaggi` (
+  `id_Personaggio` int(11) NOT NULL,
+  `nota` varchar(254) DEFAULT NULL,
+  PRIMARY KEY (`id_Personaggio`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personaggi`
+--
+
+LOCK TABLES `personaggi` WRITE;
+/*!40000 ALTER TABLE `personaggi` DISABLE KEYS */;
+INSERT INTO `personaggi` VALUES (1,'Personaggio 1');
+/*!40000 ALTER TABLE `personaggi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `storie`
+--
+
+DROP TABLE IF EXISTS `storie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `storie` (
+  `id_Storie` int(11) NOT NULL AUTO_INCREMENT,
+  `id_Utente` int(11) NOT NULL,
+  `trama` varchar(254) NOT NULL,
+  `id_Personaggio` int(11) NOT NULL,
+  `id_Categoria` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id_Storie`),
+  KEY `id_Categoria_fk_idx` (`id_Categoria`),
+  KEY `id_Personaggio_fk_idx` (`id_Personaggio`),
+  KEY `id_Utente_fk_idx` (`id_Utente`),
+  CONSTRAINT `id_Categoria_fk` FOREIGN KEY (`id_Categoria`) REFERENCES `categorie` (`id`),
+  CONSTRAINT `id_Personaggio_fk` FOREIGN KEY (`id_Personaggio`) REFERENCES `personaggi` (`id_Personaggio`),
+  CONSTRAINT `id_Utente_fk` FOREIGN KEY (`id_Utente`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `storie`
+--
+
+LOCK TABLES `storie` WRITE;
+/*!40000 ALTER TABLE `storie` DISABLE KEYS */;
+INSERT INTO `storie` VALUES (1,1,'commento  non so',1,1),(2,1,'dsadasdsa',1,2);
+/*!40000 ALTER TABLE `storie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user` (
+  `username` varchar(16) NOT NULL,
+  `usertype` varchar(255) DEFAULT NULL,
+  `password` varchar(32) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('admin','ADMIN','admin',1),('user','USER','user',2),('prova','User','nonso',3),('caffe','admin','hobisogno',4),('prova','user','si',5),('pippo','user','cazz',6),('asdrubale','user','4444',7),('Abdul','USER','abdalah',8),('admin','USER','admin',9),('user','USER','user',10),('a','USER','a',11),('a','USER','a',12),('artem','ADMIN','mat',13),('artem','USER','mat',14),('ccc','user','ccc',15),('lmlm','USER','lmlm',16);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-07-04 15:09:06
